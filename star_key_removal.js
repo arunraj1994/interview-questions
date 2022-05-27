fa494178bdaf84a744f54d0bbbe85678345ef0ad
arunraj1994 <arunrajbe94@gmail.com>
2022-05-27 20:48:51 +0530

    Create star_key_removal.js

---
+++ star_key_removal.js
@@ -0,0 +1,41 @@
+let data = {
+ "key1": {
+    "a" : {
+       "*": {
+          "key11": {
+             "aa": 1
+           }
+        }
+     }
+  },
+  "*": {
+     "key2": {
+         'b': 5
+      }
+  }
+}
+let finalObj = {};
+let temp;
+function constructObj(data, finalObj) {
+   for(let a in data) {
+      if(a === '*') {
+         return constructObj(data[a], {});
+      } else if(typeof data[a] === 'object' && data[a] !== null) {
+             finalObj[a] = constructObj(data[a], {});
+             return finalObj;
+      } else {
+         finalObj[a] = data[a];
+         return finalObj;
+      }
+   }
+   return finalObj;
+}
+
+let f = {};
+for(let b in data) {
+   if(b === '*') {
+     f = Object.assign({}, f, constructObj(data[b], {}))
+   } else {
+     f[b] = constructObj(data[b], {});
+   }
+}
