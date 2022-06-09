d9e0025bbd9dcd7820bb38c90033656ae2f66b2f
arunraj1994 <arunrajbe94@gmail.com>
2022-06-09 15:30:59 +0530

    Update diagonal_element_traversal.js

--- diagonal_element_traversal.js
+++ diagonal_element_traversal.js
@@ -1,16 +1,15 @@
+
 function printDiagonalTraversal(nums)
 {
     
-    // Store elements in desired order
     let v = [];                          
-        
-    // Store every element on the basis
-    // of sum of index (i + j)
+    let col = nums[0].length;
+    let row = nums.length
     let x;
-    for(let i = 0; i < nums[0].length ; i++)
+    for(let i = 0; i < col ; i++)
     {
         x = 0;
-        for(let j = nums.length - 1; j >=0 ; j--)
+        for(let j = row - 1; j >=0 ; j--)
         {
             if(!v[i+x]) {
               v[i+x] = [];
@@ -20,15 +19,11 @@ function printDiagonalTraversal(nums)
         }
         
     }
-    
-    // Print the stored result
     console.log(v)
 }
 
-// Given vector of vectors arr
 let arr = [ [ 1, 2, 3, 11, 14 ],
             [ 4, 5, 6, 12, 15 ],
             [ 7, 8, 9, 13, 16 ] ];
        
-// Function Call
 printDiagonalTraversal(arr);
